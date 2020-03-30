0a8df35e3185bc99908636b3d05d2858c735b661
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-03-30 16:24:42 +0200

    Put, Post and delete methods are working

--- UserModule.cs
+++ UserModule.cs
@@ -5,7 +5,8 @@ using Nancy;
 using System.Data.SqlClient;
 using System.Linq;
 using Newtonsoft.Json;
-using System.Linq;
+using System.Net.Http;
+using System.Net;
 
 namespace CreateApi_NancyFk
 {
@@ -18,27 +19,66 @@ namespace CreateApi_NancyFk
         {
             Context = new NancyContext();
 
-            // Ci-dessous: exemple du net, https://www.c-sharpcorner.com/article/generating-api-document-in-nancy/ qui ne fonctionne pas.
-            // En effet, il semblerait que Nancy ne puisse effectuer toutes les requêtes notamment les requêtes Put et Delete.
-            // Les réponses des requêtes Get, Put, Delete et Post on été testées sur le navigateur, Postman et en ligne de commande. Seules les requêtes Get exécutent la commande.
-            /*Delete("/{productid}", _ =>
-            {
-                return Response.AsText("Delete product");
-            }, null, "DeleteProductByProductId");*/
-
             Get("/Users", parameters => GetAllUsers());
 
             Get("/Users/{UserId}", parameters => GetUser(parameters.UserId));
 
-            //Delete("/Delete/{UserId}", parameters => DeleteUser(parameters.UserId));
-            Get("/Delete/{UserId}", parameters => DeleteUser(parameters.UserId));
+            Delete("/Delete/{UserId}", parameters =>
+            {
+                int id = parameters.UserId;
+                bool isUserRecorded = Context.Users.ToList().Where(x => x.UserIdNum == id).Any();
+                
+                if (isUserRecorded)
+                {
+                    User userToRemove = (from u in Context.Users
+                                         where u.UserIdNum == id
+                                         select u).First();
+                    Context.Users.Remove(userToRemove);
+                    Context.SaveChanges();
+                    var response = Response.AsJson(Nancy.HttpStatusCode.OK);
+                    return response;
+                }
+                else
+                {
+                    var response = Response.AsJson(Nancy.HttpStatusCode.NotFound);
+                    return response;
+                }
+            });
 
-            //Put("/Users/{UserName}/{Password}", parameters => PutNewUser(parameters.UserName, parameters.Password));
-            Get("/Users/{UserName}/{Password}", parameters => PutNewUser(parameters.UserName, parameters.Password));
+            Put("/Users/{UserName}/{Password}", parameters =>
+            {
+                string userName = parameters.UserName;
+                string password = parameters.Password;
+                var maxContextId = (from i in Context.Users
+                                    orderby i.UserIdNum descending
+                                    select i.UserIdNum).First();
 
+                User user = new User() { UserName = userName, Password = password, UserIdNum = maxContextId + 1 };
+                Context.Add(user);
+                Context.SaveChanges();
+                var response = Response.AsJson(Nancy.HttpStatusCode.OK);
+                return response;
+            });
+            
+            Post("/authentify/{UserName}/{Password}", parameters =>
+            {
+                string userName = parameters.UserName;
+                string password = parameters.Password;
+                var user = (from i in Context.Users
+                            where i.UserName == userName
+                            select i).First();
 
-            //Post("/authentify", parameters => Authentify(parameters.UserName, parameters.Password));  
-            Get("/authentify/{UserName}/{Password}", parameters => Authentify(parameters.UserName, parameters.Password));
+                if(password == user.Password)
+                {
+                    var response = Response.AsJson(Nancy.HttpStatusCode.OK);
+                    return response;
+                }
+                else
+                {
+                    var response = Response.AsJson(Nancy.HttpStatusCode.Forbidden);
+                    return response;
+                }
+            });  
         }
 
         private dynamic ServeHome(object manyParameters)
@@ -62,47 +102,5 @@ namespace CreateApi_NancyFk
             string user1 = JsonConvert.SerializeObject(user);
             return user1;
         }
-
-        public string DeleteUser(object userId)
-        {
-            User user = (from u in Context.Users
-                         where u.UserIdNum == Convert.ToInt32(userId)
-                         select u).First();
-            Context.Users.Remove(user);
-            Context.SaveChanges();
-            return "Deletion OK";
-        }
-
-        public string PutNewUser(object userName, object password)
-        {
-            int maxId = (from u in Context.Users
-                         orderby u.UserIdNum descending
-                         select u.UserIdNum).First();
-            User user = new User() { UserName = userName.ToString(), Password = password.ToString(), UserIdNum = maxId+1};
-            Context.Add(user);
-            Context.SaveChanges();
-            return "Insertion OK";
-        }
-
-        public string Authentify(object userName, object password)
-        {
-            User user = (from u in Context.Users
-                        where u.UserName == userName.ToString()
-                        select u).First();
-            Authentified ath = new Authentified();
-            if (password.ToString() == user.Password)
-            {
-                ath.Welcome = "Welcome";
-                ath.IsAuthentified = true;
-            }
-            else
-            {
-                ath.Welcome = "Not welcome";
-                ath.IsAuthentified = false;
-            }
-            string authentification = JsonConvert.SerializeObject(ath);
-            return authentification;
-        }
-
     }
 }
